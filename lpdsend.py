70e984b8da9d989cb7e2c434a0c4e349e177cfe8
Michael Ilsaas <mikeri@mikeri.net>
2019-01-15 18:07:25 +0100

    Use username and hostname from system

--- lpdsend.py
+++ lpdsend.py
@@ -2,6 +2,7 @@
 import sys
 import socket
 import random
+from getpass import getuser
 
 if sys.stdin.isatty():
     print("Specify print data as stdin. Examples:")
@@ -16,15 +17,17 @@ except IndexError:
     print("lpdsend.py 192.168.0.20 < testpage.pdf")
     exit()
 
+user = bytes(getuser().encode('ascii', 'ignore'))
+hostname = bytes(socket.gethostname().encode('ascii', 'ignore'))
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connection.connect(printer_address)
 job_number = bytes(f"{random.randint(1,100):03d}", 'ascii')
 print_data = sys.stdin.buffer.read()
-control_file = b'''Hlpdsendhost
-Plpdsend-user
+control_file = b'''H''' + hostname + b'''
+P''' + user + b'''
 JLPD Test Page
-ldfA''' + job_number + b'''lpdsendhost
-UdfA''' + job_number + b'''lpdsendhost
+ldfA''' + job_number + hostname + b'''
+UdfA''' + job_number + hostname + b'''
 NLPD Test Page
 '''
 data_length = len(print_data)
@@ -58,16 +61,16 @@ def response(connection):
         return False
 
 print("Starting LPD connection...")
-lpd_send(lpd_command('02', [b'lp']))
-# print("Sending control file recv command")
-# length_string = bytes(str(control_length), 'ascii')
-# lpd_send(lpd_command('02', [length_string, b"cfA" + job_number + b"lpdsendhost"]))
-# print("Sending control data...")
-# connection.send(control_file)
+lpd_send(lpd_command('02', [b'lptest']))
+print("Sending control file recv command")
+length_string = bytes(str(control_length), 'ascii')
+lpd_send(lpd_command('02', [length_string, b"cfA" + job_number + hostname]))
+print("Sending control data...")
+lpd_send(control_file + b'\x00')
 print("Sending job recv command")
 length_string = bytes(str(data_length), 'ascii')
-lpd_send(lpd_command('03', [length_string, b"dfA" + job_number + b"lpdsendhost"]))
+lpd_send(lpd_command('03', [length_string, b"dfA" + job_number + hostname]))
 print("Sending print data...")
-connection.send(print_data)
+connection.send(print_data + b'\x00')
 connection.close()
 print("Job sent successfully.")
